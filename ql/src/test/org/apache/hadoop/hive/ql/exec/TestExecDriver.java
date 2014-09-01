difin/difin_hive

HIVE-6939 : TestExecDriver.testMapRedPlan3 fails on hadoop-2 (Jason Dere via Ashutosh Chauhan)
git-svn-id: https://svn.apache.org/repos/asf/hive/trunk@1589219 13f79535-47bb-0310-9956-ffa450edef68

ql/src/test/org/apache/hadoop/hive/ql/exec/TestExecDriver.java
@@ -316,7 +316,7 @@ public class TestExecDriver extends TestCase {
 
     addMapWork(mr, src2, "b", op2);
     ReduceWork rWork = new ReduceWork();
-    rWork.setNumReduceTasks(Integer.valueOf(5));
+    rWork.setNumReduceTasks(Integer.valueOf(1));
     rWork.setNeedsTagging(true);
     rWork.setKeyDesc(op1.getConf().getKeySerializeInfo());
     rWork.getTagToValueDesc().add(op1.getConf().getValueSerializeInfo());

WANdisco/amplab-hive

Conflicts:
	ql/src/test/org/apache/hadoop/hive/ql/exec/TestExecDriver.java

ql/src/test/org/apache/hadoop/hive/ql/exec/TestExecDriver.java
@@ -281,7 +281,7 @@ public class TestExecDriver extends TestCase {
    */
   @SuppressWarnings("unchecked")
   private void populateMapRedPlan3(Table src, Table src2) throws SemanticException {
-    mr.setNumReduceTasks(Integer.valueOf(5));
+    mr.setNumReduceTasks(Integer.valueOf(1));
     mr.setNeedsTagging(true);
     ArrayList<String> outputColumns = new ArrayList<String>();
     for (int i = 0; i < 2; i++) {