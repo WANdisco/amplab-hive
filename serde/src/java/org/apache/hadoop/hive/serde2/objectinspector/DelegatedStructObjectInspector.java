8368e4dfad5bc34edacaaffbf53cdd7644fc8d64
Oleg Danilov <oleg.danilov@wandisco.com>
2014-11-06 04:30:54 +0300

    Fixed memory leak in OI cache caused by lot of queries

--- serde/src/java/org/apache/hadoop/hive/serde2/objectinspector/DelegatedStructObjectInspector.java
+++ serde/src/java/org/apache/hadoop/hive/serde2/objectinspector/DelegatedStructObjectInspector.java
@@ -90,4 +90,18 @@ public class DelegatedStructObjectInspector extends StructObjectInspector {
   public Category getCategory() {
     return delegate.getCategory();
   }
+
+  public int hashCode() {
+    return delegate.hashCode();
+  }
+
+  public boolean equals(Object obj) {
+    if (!(obj instanceof DelegatedStructObjectInspector)) {
+      return false;
+    }
+    if (obj == this) {
+      return true;
+    }
+    return delegate.equals(((DelegatedStructObjectInspector)obj).delegate);
+  }
 }
