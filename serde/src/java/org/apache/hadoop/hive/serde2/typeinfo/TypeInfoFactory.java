difin/difin_hive

HIVE-10023 : Fix more cache related concurrency issue [Spark Branch] (Jimmy Xiang via Szehon)
git-svn-id: https://svn.apache.org/repos/asf/hive/branches/spark@1667905 13f79535-47bb-0310-9956-ffa450edef68

serde/src/java/org/apache/hadoop/hive/serde2/typeinfo/TypeInfoFactory.java
@@ -22,13 +22,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hive.common.type.HiveChar;
-import org.apache.hadoop.hive.common.type.HiveVarchar;
 import org.apache.hadoop.hive.common.type.HiveDecimal;
+import org.apache.hadoop.hive.common.type.HiveVarchar;
 import org.apache.hadoop.hive.serde.serdeConstants;
-import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector.PrimitiveCategory;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorUtils;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorUtils.PrimitiveTypeEntry;
 
@@ -40,7 +37,6 @@ import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectIn
  * objects that represents the same type.
  */
 public final class TypeInfoFactory {
-  private static Log LOG = LogFactory.getLog(TypeInfoFactory.class);
 
   private TypeInfoFactory() {
     // prevent instantiation
@@ -111,7 +107,10 @@ public final class TypeInfoFactory {
       throw new RuntimeException("Error creating PrimitiveTypeInfo instance for " + typeName);
     }
 
-    cachedPrimitiveTypeInfo.put(typeName, result);
+    PrimitiveTypeInfo prev = cachedPrimitiveTypeInfo.putIfAbsent(typeName, result);
+    if (prev != null) {
+      result = prev;
+    }
     return result;
   }
 
@@ -199,7 +198,10 @@ public final class TypeInfoFactory {
     TypeInfo result = cachedStructTypeInfo.get(signature);
     if (result == null) {
       result = new StructTypeInfo(names, typeInfos);
-      cachedStructTypeInfo.put(signature, result);
+      TypeInfo prev = cachedStructTypeInfo.putIfAbsent(signature, result);
+      if (prev != null) {
+        result = prev;
+      }
     }
     return result;
   }
@@ -211,7 +213,10 @@ public final class TypeInfoFactory {
     TypeInfo result = cachedUnionTypeInfo.get(typeInfos);
     if (result == null) {
       result = new UnionTypeInfo(typeInfos);
-      cachedUnionTypeInfo.put(typeInfos, result);
+      TypeInfo prev = cachedUnionTypeInfo.putIfAbsent(typeInfos, result);
+      if (prev != null) {
+        result = prev;
+      }
     }
     return result;
   }
@@ -222,7 +227,10 @@ public final class TypeInfoFactory {
     TypeInfo result = cachedListTypeInfo.get(elementTypeInfo);
     if (result == null) {
       result = new ListTypeInfo(elementTypeInfo);
-      cachedListTypeInfo.put(elementTypeInfo, result);
+      TypeInfo prev = cachedListTypeInfo.putIfAbsent(elementTypeInfo, result);
+      if (prev != null) {
+        result = prev;
+      }
     }
     return result;
   }
@@ -238,7 +246,10 @@ public final class TypeInfoFactory {
     TypeInfo result = cachedMapTypeInfo.get(signature);
     if (result == null) {
       result = new MapTypeInfo(keyTypeInfo, valueTypeInfo);
-      cachedMapTypeInfo.put(signature, result);
+      TypeInfo prev = cachedMapTypeInfo.putIfAbsent(signature, result);
+      if (prev != null) {
+        result = prev;
+      }
     }
     return result;
   }

WANdisco/amplab-hive

serde/src/java/org/apache/hadoop/hive/serde2/typeinfo/TypeInfoFactory.java
@@ -48,7 +48,10 @@ public final class TypeInfoFactory {
     TypeInfo result = cachedPrimitiveTypeInfo.get(typeName);
     if (result == null) {
       result = new PrimitiveTypeInfo(typeName);
-      cachedPrimitiveTypeInfo.put(typeName, result);
+      TypeInfo prev = cachedPrimitiveTypeInfo.putIfAbsent(typeName, result);
+      if (prev != null) {
+        result = prev;
+      }
     }
     return result;
   }
@@ -95,7 +98,10 @@ public final class TypeInfoFactory {
     TypeInfo result = cachedStructTypeInfo.get(signature);
     if (result == null) {
       result = new StructTypeInfo(names, typeInfos);
-      cachedStructTypeInfo.put(signature, result);
+      TypeInfo prev = cachedStructTypeInfo.putIfAbsent(signature, result);
+      if (prev != null) {
+        result = prev;
+      }
     }
     return result;
   }
@@ -107,7 +113,10 @@ public final class TypeInfoFactory {
     TypeInfo result = cachedUnionTypeInfo.get(typeInfos);
     if (result == null) {
       result = new UnionTypeInfo(typeInfos);
-      cachedUnionTypeInfo.put(typeInfos, result);
+      TypeInfo prev = cachedUnionTypeInfo.putIfAbsent(typeInfos, result);
+      if (prev != null) {
+        result = prev;
+      }
     }
     return result;
   }
@@ -118,7 +127,10 @@ public final class TypeInfoFactory {
     TypeInfo result = cachedListTypeInfo.get(elementTypeInfo);
     if (result == null) {
       result = new ListTypeInfo(elementTypeInfo);
-      cachedListTypeInfo.put(elementTypeInfo, result);
+      TypeInfo prev = cachedListTypeInfo.putIfAbsent(elementTypeInfo, result);
+      if (prev != null) {
+        result = prev;
+      }
     }
     return result;
   }
@@ -134,7 +146,10 @@ public final class TypeInfoFactory {
     TypeInfo result = cachedMapTypeInfo.get(signature);
     if (result == null) {
       result = new MapTypeInfo(keyTypeInfo, valueTypeInfo);
-      cachedMapTypeInfo.put(signature, result);
+      TypeInfo prev = cachedMapTypeInfo.putIfAbsent(signature, result);
+      if (prev != null) {
+        result = prev;
+      }
     }
     return result;
   };